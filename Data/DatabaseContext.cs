661bef60e09dbbb002ca8c19b7753d4454e3b4f4
k1ngsengson <King.sengson@outlook.com>
2020-07-04 12:07:21 +0800

    #2 create data project

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Location> Locations { get; set; }
+
+        private readonly string _databasePath;
+
+        public DatabaseContext(string databasePath)
+        {
+            _databasePath = databasePath;
+
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(string.Format("Filename={0}", _databasePath));
+        }
+    }
+}
