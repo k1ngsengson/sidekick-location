39048d2300204ef04bd1ee260f6177938dcfa27d
k1ngsengson <King.sengson@outlook.com>
2020-07-05 20:56:09 +0800

    remove INotifyPropertyChanged and add properties to BaseViewModel

--- Core/ViewModels/BaseViewModel.cs
+++ Core/ViewModels/BaseViewModel.cs
@@ -1,9 +1,21 @@
 ﻿using MvvmCross.ViewModels;
-using System.ComponentModel;
 
 namespace Core.ViewModels
 {
-    public class BaseViewModel: MvxViewModel, INotifyPropertyChanged
+    public class BaseViewModel: MvxViewModel
     {
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        string title = string.Empty;
+        public string Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
     }
 }
