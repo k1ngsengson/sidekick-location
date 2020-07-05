63d6ab61ba510f4228a2f882f5e4ea713efa04ad
k1ngsengson <King.sengson@outlook.com>
2020-07-06 05:46:24 +0800

    add common project

---
+++ Common/StringConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace Common
+{
+    public static class StringConstants
+    {
+        public const string GoogleApiPath = "https://maps.googleapis.com/maps/"; //Adding country:us limits results to us
+
+        public const string GooglePlacesApiGeocodePath = "https://maps.googleapis.com/maps/api/place/details/json?son?key={0}&placeid={1}"; //Adding country:us limits results to us
+
+        public const string GooglePlacesApiKey = "AIzaSyCo_xvOH0nqR6RYF01_b_bnqI2V9QK7xqs";
+    }
+}
