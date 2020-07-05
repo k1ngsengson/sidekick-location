12f47ce7314c9388e32fc6a1eb301b65513c3883
k1ngsengson <King.sengson@outlook.com>
2020-07-06 01:57:18 +0800

    rename to LocationListView

--- Droid/Views/LocationListView.cs
+++ Droid/Views/LocationListView.cs
@@ -1,19 +1,26 @@
 ﻿
 using Android.App;
 using Android.OS;
+using Android.Support.Design.Widget;
+using Android.Views;
 using Core.ViewModels;
 using MvvmCross.Platforms.Android.Views;
+using System;
 
 namespace Droid.Views
 {
  
     [Activity(MainLauncher = true, Label = "Locations")]
-    public class LocationsView : MvxActivity<LocationViewModel>
+    public class LocationListView : MvxActivity<LocationListViewModel>
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.LocationList);
+
+
         }
+
+
     }
 }
