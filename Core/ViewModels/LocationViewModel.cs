264bb29b20cd0f017f1e3d5b2168fe57b963c846
k1ngsengson <King.sengson@outlook.com>
2020-07-05 20:55:46 +0800

    rename to LocationViewModel

---
+++ Core/ViewModels/LocationViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Core.Services;
+using Data.Entities;
+using System.Collections.ObjectModel;
+
+namespace Core.ViewModels
+{
+    public class LocationViewModel : BaseViewModel
+    {
+        private readonly ILocationsService _service;
+
+        public LocationViewModel(ILocationsService service)
+        {
+            _service = service;
+            LoadData();
+            Title = "Locations";
+            IsBusy = false;
+        }
+
+        private ObservableCollection<Location> _locations;
+        public ObservableCollection<Location> Locations
+        {
+            get { return _locations; }
+            set
+            {
+                _locations = value;
+                RaisePropertyChanged(() => Locations);
+            }
+        }
+
+        public async void LoadData()
+        {
+            Locations = new ObservableCollection<Location>(await _service.GetLocationsAsync());
+
+        }
+    }
+}
