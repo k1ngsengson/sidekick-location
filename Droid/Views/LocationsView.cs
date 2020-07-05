5e10857e61802e1a7fff51f8438b7a52d2360a22
k1ngsengson <King.sengson@outlook.com>
2020-07-05 20:57:38 +0800

    Add LocationsView

---
+++ Droid/Views/LocationsView.cs
@@ -0,0 +1,19 @@
+﻿
+using Android.App;
+using Android.OS;
+using Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Droid.Views
+{
+ 
+    [Activity(MainLauncher = true, Label = "Locations")]
+    public class LocationsView : MvxActivity<LocationViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LocationList);
+        }
+    }
+}
