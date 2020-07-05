a4d26a9762a4ee51ef28a3347e12a762048285c3
k1ngsengson <King.sengson@outlook.com>
2020-07-06 01:57:25 +0800

    add LocationsItemView

---
+++ Droid/Views/LocationItemView.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.OS;
+using Core.ViewModels;
+using MvvmCross.Platforms.Android.Views;
+
+namespace Droid.Views
+{
+    [Activity(Label = "Add Location")]
+    public class LocationsItemView : MvxActivity<LocationItemViewModel>
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.LocationItem);
+        }
+    }
+}
