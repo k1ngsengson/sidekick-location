1f7ed6b711b10a4e0e0df5308b6d84a333cb81d2
k1ngsengson <King.sengson@outlook.com>
2020-07-06 15:55:27 +0800

    add mapper

---
+++ Core/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using Core.Models;
+using Data.Entities;
+
+namespace Core
+{
+    public static class Mapper
+    {
+        public static AddressInfo ToAddressInfo(this Location entity)
+        {
+            var model = new AddressInfo()
+            {
+                Address = entity.Address,
+                Longitude = entity.Longitude,
+                Latitude = entity.Latitude,
+                Json = entity.Json
+            };
+
+            return model;
+        }
+
+        public static Location ToLocation(this AddressInfo model)
+        {
+            var entity = new Location()
+            {
+                Address = model.Address,
+                Longitude = model.Longitude,
+                Latitude = model.Latitude,
+                Json = model.Json
+            };
+
+            return entity;
+        }
+    }
+}
