0c62a81340196b856b1bb261aaaf82b8db1b17e3
k1ngsengson <King.sengson@outlook.com>
2020-07-06 20:14:33 +0800

    Create Entity project

---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? CreatedOn { get; set; }
+        public DateTime? ModifiedOn { get; set; }
+    }
+}
