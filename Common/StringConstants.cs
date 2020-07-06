8d9e22b7239ff7f042074c728c8474b7b40207fa
k1ngsengson <King.sengson@outlook.com>
2020-07-06 20:12:04 +0800

    change name const strings

--- Common/StringConstants.cs
+++ Common/StringConstants.cs
@@ -2,8 +2,10 @@
 {
     public static class StringConstants
     {
-        public const string GoogleApiPath = "https://maps.googleapis.com/maps/"; //Adding country:us limits results to us
+        public const string GOOGLE_API_PATH = "https://maps.googleapis.com/maps/"; //Adding country:us limits results to us
 
-        public const string GooglePlacesApiKey = "AIzaSyCo_xvOH0nqR6RYF01_b_bnqI2V9QK7xqs";
+        public const string GOOGLE_API_KEY = "AIzaSyCo_xvOH0nqR6RYF01_b_bnqI2V9QK7xqs";
+
+        public const string STORAGE = "SimpleStorage";
     }
 }
