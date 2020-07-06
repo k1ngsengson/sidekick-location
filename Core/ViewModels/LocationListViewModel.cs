c372468c0802875ce0cd8705df006ef9e3b4f546
k1ngsengson <King.sengson@outlook.com>
2020-07-06 15:59:32 +0800

    add visibility properties

--- Core/ViewModels/LocationListViewModel.cs
+++ Core/ViewModels/LocationListViewModel.cs
@@ -4,6 +4,7 @@ using MvvmCross.Commands;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace Core.ViewModels
@@ -18,9 +19,34 @@ namespace Core.ViewModels
             _service = service;
             _navigationService = navigationService;
 
-            LoadData();
+            LoadData().ConfigureAwait(false);
+
+            MapVisible = true;
+            ListVisible = false;
         }
 
+        #region Properties
+        private bool _mapVisible;
+        public bool MapVisible
+        {
+            get { return _mapVisible; }
+            set
+            {
+                _mapVisible = value;
+                RaisePropertyChanged(() => MapVisible);
+            }
+        }
+
+        private bool _listVisible;
+        public bool ListVisible
+        {
+            get { return _listVisible; }
+            set
+            {
+                _listVisible = value;
+                RaisePropertyChanged(() => ListVisible);
+            }
+        }
 
         private ObservableCollection<AddressInfo> _addresses;
         public ObservableCollection<AddressInfo> Addresses
@@ -32,25 +58,54 @@ namespace Core.ViewModels
                 RaisePropertyChanged(() => Addresses);
             }
         }
+        #endregion
 
-        public async void LoadData()
+        #region Methods
+        public async Task LoadData()
         {
             Addresses = new ObservableCollection<AddressInfo>(await _service.GetLocationsAsync());
         }
-
-        public ICommand RedirectToAddLocationCommand
-        {
-            get { return new MvxCommand(RedirectToAddLocation); }
-        }
-
-        public async void RedirectToAddLocation()
-        {
-            await _navigationService.Navigate<LocationItemViewModel>();
-        }
-
         public override void Prepare(AddressInfo addressInfo)
         {
             Addresses.Add(addressInfo);
         }
+        #endregion
+
+        #region Commands
+        public ICommand RedirectToAddLocationCommand
+        {
+            get { return new MvxCommand(OnRedirectToAddLocation); }
+        }
+
+        public ICommand ShowMapCommand
+        {
+            get { return new MvxCommand(OnShowMapCommand); }
+        }
+        public ICommand ShowListCommand
+        {
+            get { return new MvxCommand(OnShowListCommand); }
+        }
+        #endregion
+
+        #region Events
+        public void OnRedirectToAddLocation()
+        {
+            _navigationService.Navigate<LocationItemViewModel>().ConfigureAwait(false);
+        }
+
+        public void OnShowMapCommand()
+        {
+            MapVisible = true;
+            ListVisible = false;
+        }
+
+        public void OnShowListCommand()
+        {
+            MapVisible = false;
+            ListVisible = true;
+        }
+        #endregion
+
+        
     }
 }
