8801a33e753c2b049dae7ea84bb25f401fda9d43
k1ngsengson <King.sengson@outlook.com>
2020-07-06 15:59:11 +0800

    remove city, state and zipcode

--- Data/Entities/Location.cs
+++ Data/Entities/Location.cs
@@ -3,15 +3,8 @@
     public class Location: BaseEntity
     {
         public string Address { get; set; }
-
-        public string City { get; set; }
-
-        public string State { get; set; }
-
-        public string ZipCode { get; set; }
-
         public double Longitude { get; set; }
-
         public double Latitude { get; set; }
+        public string Json { get; set; }
     }
 }
