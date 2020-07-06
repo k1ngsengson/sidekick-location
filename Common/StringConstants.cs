d7acf6264e68eafe989807f6d83942e004e753ea
k1ngsengson <King.sengson@outlook.com>
2020-07-06 15:53:56 +0800

    remove GooglePlacesApiGeocodePath

--- Common/StringConstants.cs
+++ Common/StringConstants.cs
@@ -4,8 +4,6 @@
     {
         public const string GoogleApiPath = "https://maps.googleapis.com/maps/"; //Adding country:us limits results to us
 
-        public const string GooglePlacesApiGeocodePath = "https://maps.googleapis.com/maps/api/place/details/json?son?key={0}&placeid={1}"; //Adding country:us limits results to us
-
         public const string GooglePlacesApiKey = "AIzaSyCo_xvOH0nqR6RYF01_b_bnqI2V9QK7xqs";
     }
 }
